5585f1fe3155fb9177f72154afe1717bff8758b0
artkeller <artkeller@gmx.de>
2025-07-25 19:26:17 +0200

    Update ManualDE_REDirectionControl.h

--- src/ManualDE_REDirectionControl.h
+++ src/ManualDE_REDirectionControl.h
@@ -1,1 +1,28 @@
+#ifndef MANUAL_DE_RE_DIRECTION_CONTROL_H
+#define MANUAL_DE_RE_DIRECTION_CONTROL_H
 
+#include "RS485DirectionControl.h"
+#include <Arduino.h>
+
+class ManualDE_REDirectionControl : public RS485DirectionControl {
+private:
+    int _deRePin;
+
+public:
+    ManualDE_REDirectionControl(int deRePin) : _deRePin(deRePin) {}
+
+    void begin() override {
+        pinMode(_deRePin, OUTPUT);
+        setReceiveMode();
+    }
+
+    void setTransmitMode() override {
+        digitalWrite(_deRePin, HIGH);
+    }
+
+    void setReceiveMode() override {
+        digitalWrite(_deRePin, LOW);
+    }
+};
+
+#endif // MANUAL_DE_RE_DIRECTION_CONTROL_H
