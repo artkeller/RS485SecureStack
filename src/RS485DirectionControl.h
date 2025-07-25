508df935dbc49e87c74d2508267241909e320d00
artkeller <artkeller@gmx.de>
2025-07-25 19:28:37 +0200

    Update RS485DirectionControl.h

--- src/RS485DirectionControl.h
+++ src/RS485DirectionControl.h
@@ -1,1 +1,12 @@
+#ifndef RS485_DIRECTION_CONTROL_H
+#define RS485_DIRECTION_CONTROL_H
 
+class RS485DirectionControl {
+public:
+    virtual ~RS485DirectionControl() = default;
+    virtual void begin() = 0;
+    virtual void setTransmitMode() = 0;
+    virtual void setReceiveMode() = 0;
+};
+
+#endif // RS485_DIRECTION_CONTROL_H
