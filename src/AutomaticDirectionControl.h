2110b3e413a3e980b7a0b980c0c306f932e849ab
artkeller <artkeller@gmx.de>
2025-07-25 19:24:54 +0200

    Update AutomaticDirectionControl.h

--- src/AutomaticDirectionControl.h
+++ src/AutomaticDirectionControl.h
@@ -1,1 +1,14 @@
+#ifndef AUTOMATIC_DIRECTION_CONTROL_H
+#define AUTOMATIC_DIRECTION_CONTROL_H
 
+#include "RS485DirectionControl.h"
+
+class AutomaticDirectionControl : public RS485DirectionControl {
+public:
+    AutomaticDirectionControl() {}
+    void begin() override {}
+    void setTransmitMode() override {}
+    void setReceiveMode() override {}
+};
+
+#endif // AUTOMATIC_DIRECTION_CONTROL_H
